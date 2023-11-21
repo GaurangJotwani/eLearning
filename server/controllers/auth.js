d9dc5b6731803caf16376511760992e65faab909
Gaurang <gaurangjotwani@gmail.com>
2023-11-21 00:32:29 -0800

    Login API route

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,10 +1,39 @@
 import User from "../models/user";
 import { hashPassword, comparePassword } from "../utils/auth";
+import jwt from "jsonwebtoken";
+
+export const login = async (req, res) => {
+  try {
+    console.log(req.body);
+    const { email, password } = req.body;
+
+    //check if our db has user with that email
+    const user = await User.findOne({ email }).exex();
+    if (!user) return res.status(400).send("No user found");
+
+    const match = await comparePassword(password, user.password);
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    user.password = undefined;
+
+    // send token in cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      // secure: true
+    });
+
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Error. Try again");
+  }
+};
 
 export const register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    console.log(req.body);
     //validation
     if (!name) return res.status(400).send("Name is required");
     if (!password || password.length < 6) {
