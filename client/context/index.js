023a1d363d5860ad99a4b5d0eb923768f27b6bfb
Gaurang <gaurangjotwani@gmail.com>
2023-11-21 16:02:15 -0800

    CSRF package

--- client/context/index.js
+++ client/context/index.js
@@ -1,4 +1,6 @@
 import { useReducer, createContext, useEffect } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 const initialState = {
   user: null,
@@ -7,7 +9,6 @@ const initialState = {
 const Context = createContext();
 
 const rootReducer = (state, action) => {
-  console.log(action);
   switch (action.type) {
     case "LOGIN":
       console.log({ ...state, user: action.payload });
@@ -22,12 +23,48 @@ const rootReducer = (state, action) => {
 // context provider
 const Provider = ({ children }) => {
   const [state, dispatch] = useReducer(rootReducer, initialState);
+  const router = useRouter();
 
   useEffect(() => {
     dispatch({
       type: "LOGIN",
       payload: JSON.parse(window.localStorage.getItem("user")),
     });
+  }, []);
+
+  axios.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    function (error) {
+      // any status codes falls out of the range of 2XX cause this function to trigger
+      let res = error.response;
+      if (res.status === 401 && res.config && !res.config.__isRetryRequest) {
+        return new Promise((resolve, reject) => {
+          axios
+            .get("/api/logout")
+            .then((data) => {
+              console.log("/401 error > logout");
+              dispatch({ type: "LOGOUT" });
+              window.localStorage.removeItem("user");
+              router.push("/login");
+            })
+            .catch((err) => {
+              console.log("AXIOS INTERCEPTOR ERROR", err);
+              reject(error);
+            });
+        });
+      }
+      return Promise.reject(error);
+    }
+  );
+
+  useEffect(() => {
+    const getCsrfToken = async () => {
+      const { data } = await axios.get("/api/csrf-token");
+      axios.defaults.headers["X-CSRF-Token"] = data.getCsrfToken;
+    };
+    getCsrfToken();
   }, []);
 
   return (
--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ import express from "express";
 import cors from "cors";
 import { readdirSync } from "fs";
 import mongoose from "mongoose";
-
+import csrf from "csurf";
 const morgan = require("morgan");
 require("dotenv").config();
+import cookieParser from "cookie-parser";
+
+const csrfProtection = csrf({ cookie: true });
 
 // create express app
 const app = express();
@@ -12,11 +15,17 @@ const app = express();
 // apply middlewares
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(morgan("dev"));
+app.use(csrfProtection);
 
 //routes
 readdirSync("./routes").map((r) => {
   app.use("/api", require(`./routes/${r}`));
+});
+
+app.get("/api/csrf-token", (req, res) => {
+  res.json({ csrfToken: req.csrfToken() });
 });
 
 mongoose
